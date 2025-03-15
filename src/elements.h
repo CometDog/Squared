bdf0ffc6c4f536541aa894f76a23fc28eb2d3eed
James <5879831+CometDog@users.noreply.github.com>
2025-03-15 02:02:49 -0400

    Fundamentally restructured the app and provided documentation

--- src/elements.h
+++ src/elements.h
@@ -1,41 +1,51 @@
-/*
-Squared
-  Copyright (C) 2015 James Downs
-  james.j.downs@icloud.com
-*/
-
+// Size of the digit boxes
 #define BOX_X 72
 #define BOX_Y 84
 
+// Animation duration and delay between in and out animations
 #define ANIM_DURATION 500
 #define ANIM_DELAY 300
 
+/**
+ * Represents a layer with a bitmap
+ */
 typedef struct
 {
   Layer *parent_layer;
   BitmapLayer *bitmap_layer;
   GBitmap *bitmap;
+} MaterialLayer;
+
+/**
+ * Represents a digit layer with a bitmap, animation, and positional props
+ */
+typedef struct
+{
+  MaterialLayer material;
   PropertyAnimation *animation;
   bool out_of_frame;
   int position;
 } DigitLayer;
 
 static Window *s_main_window;
-static Layer *s_background_layer;
+static MaterialLayer *s_background;
 static DigitLayer *s_hour1, *s_hour2, *s_minute1, *s_minute2;
-static BitmapLayer *s_material_layer;
-static GBitmap *s_material_bitmap;
 
 static AppTimer *timer = NULL;
 
-static int8_t hour;
-static int8_t hour1;
-static int8_t hour2;
-static int8_t minute1;
-static int8_t minute2;
+/**
+ * Represents the digits of a clock
+ */
+static int clock_digits[4];
 
+/**
+ * Whether the watch has not been interacted with for a while
+ */
 static bool idle = true;
 
+/**
+ * Resource IDs for each digit image
+ */
 const int DIGIT_RESOURCE_IDS[] = {
     RESOURCE_ID_T0,
     RESOURCE_ID_T1,
@@ -48,5 +58,19 @@ const int DIGIT_RESOURCE_IDS[] = {
     RESOURCE_ID_T8,
     RESOURCE_ID_T9};
 
+/**
+ * Values for the position of each digit when in and out of frame
+ */
+const struct
+{
+  int out_of_frame[2];
+  int in_frame[2];
+} DIGIT_POSITION_VALUES[4] = {
+    {{-144, 0}, {0, 0}},   // top-left
+    {{72, -168}, {72, 0}}, // top-right
+    {{0, 252}, {0, 84}},   // bottom-left
+    {{216, 84}, {72, 84}}  // bottom-right
+};
+
 static void do_animation(DigitLayer *digit_layer);
-static void update_bg(Layer *layer, GContext *ctx);
+static void update_layer(Layer *layer, GContext *context);
--- src/main.c
+++ src/main.c
@@ -1,9 +1,3 @@
-/*
-Squared
-    Copyright (C) 2015 James Downs
-    james.j.downs@icloud.com
-*/
-
 #include "pebble.h"
 #include "libs/pebble-assist.h"
 #include "elements.h"
@@ -19,7 +13,7 @@ static void anim_stopped_handler(Animation *animation, bool finished, void *cont
   if (finished)
   {
     DigitLayer *digit_layer = (DigitLayer *)context;
-    layer_mark_dirty(s_background_layer);
+    layer_mark_dirty(s_background->parent_layer);
     if (digit_layer->out_of_frame)
     {
       do_animation(digit_layer);
@@ -34,6 +28,7 @@ static void anim_stopped_handler(Animation *animation, bool finished, void *cont
  */
 static void anim_started_handler(Animation *animation, void *context)
 {
+  // Flips the out_of_frame flag as soon as the animation starts
   DigitLayer *digit_layer = (DigitLayer *)context;
   digit_layer->out_of_frame = !digit_layer->out_of_frame;
 }
@@ -48,7 +43,7 @@ static void animate_digit(DigitLayer *digit_layer, GRect start, GRect finish)
 {
   // Create and configure animation
   digit_layer->animation = property_animation_create_layer_frame(
-      digit_layer->parent_layer, &start, &finish);
+      digit_layer->material.parent_layer, &start, &finish);
   Animation *anim = (Animation *)digit_layer->animation;
 
   animation_set_handlers(anim, (AnimationHandlers){.started = anim_started_handler, .stopped = anim_stopped_handler}, digit_layer);
@@ -66,119 +61,141 @@ static void animate_digit(DigitLayer *digit_layer, GRect start, GRect finish)
  */
 static void do_animation(DigitLayer *digit_layer)
 {
-  // Define start and end positions
-  const struct
-  {
-    GRect out_of_frame;
-    GRect in_frame;
-  } positions[] = {
-      {GRect(-144, 0, BOX_X, BOX_Y), GRect(0, 0, BOX_X, BOX_Y)},   // top-left
-      {GRect(72, -168, BOX_X, BOX_Y), GRect(72, 0, BOX_X, BOX_Y)}, // top-right
-      {GRect(0, 252, BOX_X, BOX_Y), GRect(0, 84, BOX_X, BOX_Y)},   // bottom-left
-      {GRect(216, 84, BOX_X, BOX_Y), GRect(72, 84, BOX_X, BOX_Y)}  // bottom-right
-  };
+  // Define positions in and out of frame for each position
+  GRect out_of_frame = GRect(
+      DIGIT_POSITION_VALUES[digit_layer->position].out_of_frame[0],
+      DIGIT_POSITION_VALUES[digit_layer->position].out_of_frame[1],
+      BOX_X,
+      BOX_Y);
+  GRect in_frame = GRect(
+      DIGIT_POSITION_VALUES[digit_layer->position].in_frame[0],
+      DIGIT_POSITION_VALUES[digit_layer->position].in_frame[1],
+      BOX_X,
+      BOX_Y);
+
+  GRect start = digit_layer->out_of_frame
+                    ? out_of_frame
+                    : in_frame;
+  GRect finish = digit_layer->out_of_frame
+                     ? in_frame
+                     : out_of_frame;
 
   // Animate digit layer
-  animate_digit(digit_layer,
-                digit_layer->out_of_frame // Start
-                    ? positions[digit_layer->position].out_of_frame
-                    : positions[digit_layer->position].in_frame,
-                digit_layer->out_of_frame // Finish
-                    ? positions[digit_layer->position].in_frame
-                    : positions[digit_layer->position].out_of_frame);
+  animate_digit(digit_layer, start, finish);
 }
 
-static void update_bg(Layer *layer, GContext *ctx)
+/**
+ * Updates the given layer bitmap
+ * @param layer The layer to update. Unused
+ * @param context The graphics context. Unused
+ */
+static void update_layer(Layer *layer, GContext *context)
 {
+  // Prevent updating the layer if any digit is out of frame to avoid replacing digits mid-animation
   bool any_out_of_frame = s_hour1->out_of_frame || s_hour2->out_of_frame || s_minute1->out_of_frame || s_minute2->out_of_frame;
-  if (!any_out_of_frame)
-  {
-    gbitmap_destroy_safe(s_hour1->bitmap);
-    gbitmap_destroy_safe(s_hour2->bitmap);
-    gbitmap_destroy_safe(s_minute1->bitmap);
-    gbitmap_destroy_safe(s_minute2->bitmap);
+  if (any_out_of_frame)
+    return;
 
-    GRect bounds = layer_get_bounds(layer);
-    GPoint center = grect_center_point(&bounds);
+  gbitmap_destroy_safe(s_hour1->material.bitmap);
+  gbitmap_destroy_safe(s_hour2->material.bitmap);
+  gbitmap_destroy_safe(s_minute1->material.bitmap);
+  gbitmap_destroy_safe(s_minute2->material.bitmap);
 
-    s_hour1->bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[hour1]);
-    s_hour2->bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[hour2]);
-    s_minute1->bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[minute1]);
-    s_minute2->bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[minute2]);
+  s_hour1->material.bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[clock_digits[0]]);
+  s_hour2->material.bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[clock_digits[1]]);
+  s_minute1->material.bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[clock_digits[2]]);
+  s_minute2->material.bitmap = gbitmap_create_with_resource(DIGIT_RESOURCE_IDS[clock_digits[3]]);
 
-    bitmap_layer_set_bitmap(s_hour1->bitmap_layer, s_hour1->bitmap);
-    bitmap_layer_set_bitmap(s_hour2->bitmap_layer, s_hour2->bitmap);
-    bitmap_layer_set_bitmap(s_minute1->bitmap_layer, s_minute1->bitmap);
-    bitmap_layer_set_bitmap(s_minute2->bitmap_layer, s_minute2->bitmap);
-  }
+  bitmap_layer_set_bitmap(s_hour1->material.bitmap_layer, s_hour1->material.bitmap);
+  bitmap_layer_set_bitmap(s_hour2->material.bitmap_layer, s_hour2->material.bitmap);
+  bitmap_layer_set_bitmap(s_minute1->material.bitmap_layer, s_minute1->material.bitmap);
+  bitmap_layer_set_bitmap(s_minute2->material.bitmap_layer, s_minute2->material.bitmap);
 }
 
+/**
+ * Updates the time and triggers animations
+ */
 static void update_time()
 {
   time_t epoch = time(NULL);
   struct tm *t = localtime(&epoch);
 
-  if (clock_is_24h_style() == true)
+  int hour = t->tm_hour;
+  if (!clock_is_24h_style())
   {
-    hour = t->tm_hour;
-  }
-  else
-  {
-    hour = t->tm_hour % 12;
+    hour %= 12;
+    if (hour == 0)
+      hour = 12;
   }
 
-  if (clock_is_24h_style() == false && (hour == 12 || hour == 0))
-  {
-    hour1 = 1;
-    hour2 = 2;
-  }
-  else
-  {
-    hour1 = hour / 10;
-    hour2 = hour - (hour1 * 10);
-  }
-  minute1 = t->tm_min / 10;
-  minute2 = t->tm_min - (minute1 * 10);
+  // This relies on divison by assuming rounding down
+  clock_digits[0] = hour / 10;
+  clock_digits[1] = hour % 10;
+  clock_digits[2] = t->tm_min / 10;
+  clock_digits[3] = t->tm_min % 10;
 
   if (idle)
   {
-    layer_mark_dirty(s_background_layer);
-    layer_set_update_proc(s_background_layer, update_bg);
+    // When idle, just perform a non-animated update
+    layer_mark_dirty(s_background->parent_layer);
   }
   else
   {
+    // Last minute always updates
     do_animation(s_minute2);
-    if (minute2 == 0)
+
+    // If the ones position of minute is not 0, we have not rolled over yet
+    if (clock_digits[3] != 0)
+      return;
+
+    do_animation(s_minute1);
+
+    // If the tens position of minute is not 0, we have not rolled over yet
+    if (clock_digits[2] != 0)
+      return;
+
+    do_animation(s_hour2);
+
+    // If the ones position of hour is not 0, we have not rolled over yet
+    if (clock_digits[1] != 0)
+      return;
+
+    do_animation(s_hour1);
+
+    // Special case for 12-hour format when rolling from 12 to 1
+    if (!clock_is_24h_style() && clock_digits[1] == 1 && clock_digits[0] != 1)
     {
-      do_animation(s_minute1);
-      if (minute1 == 0)
-      {
-        do_animation(s_hour2);
-        if (hour2 == 0)
-        {
-          do_animation(s_hour1);
-          if (clock_is_24h_style() == false && hour2 == 1 && hour1 != 1)
-          {
-            do_animation(s_hour1);
-          }
-        }
-      }
+      do_animation(s_hour1);
     }
   }
 }
 
+/**
+ * Timer callback to set the idle flag
+ * @param data Unused
+ */
 static void timer_callback(void *data)
 {
   idle = true;
 }
 
+/**
+ * Tap handler to reset the idle timer
+ * @param axis The axis of the tap. Unused
+ * @param direction The direction of the tap. Unused
+ */
 static void tap_handler(AccelAxisType axis, int32_t direction)
 {
   idle = false;
   app_timer_cancel(timer);
+  // After 3 minutes of inactivity, set the idle flag to disable animations
   timer = app_timer_register(180 * 1000, timer_callback, NULL);
 }
 
+/**
+ * Bluetooth connection handler to vibrate on connection status change
+ * @param connected Whether the connection is established
+ */
 static void bt_handler(bool connected)
 {
   if (connected)
@@ -191,88 +208,91 @@ static void bt_handler(bool connected)
   }
 }
 
+/**
+ * Handle time tick event
+ * @param tick_time Pointer to the time structure. Unused
+ * @param units_changed The units that have changed.
+ */
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
-  update_time();
+  // Update time every minute
+  if (units_changed & MINUTE_UNIT)
+  {
+    update_time();
+  }
 }
 
+/**
+ * Main window load handler
+ * @param window The window being loaded
+ */
 static void main_window_load(Window *window)
 {
   GRect bounds = window_get_bounds(window);
 
-  s_background_layer = layer_create(bounds);
+  s_background = malloc(sizeof(MaterialLayer));
+  DigitLayer *digits[] = {
+      s_hour1 = malloc(sizeof(DigitLayer)),
+      s_hour2 = malloc(sizeof(DigitLayer)),
+      s_minute1 = malloc(sizeof(DigitLayer)),
+      s_minute2 = malloc(sizeof(DigitLayer))};
 
-  s_hour1 = (DigitLayer *)malloc(sizeof(DigitLayer));
-  s_hour2 = (DigitLayer *)malloc(sizeof(DigitLayer));
-  s_minute1 = (DigitLayer *)malloc(sizeof(DigitLayer));
-  s_minute2 = (DigitLayer *)malloc(sizeof(DigitLayer));
+  s_background->parent_layer = layer_create(bounds);
+  s_background->bitmap_layer = bitmap_layer_create(bounds);
+  s_background->bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
+  bitmap_layer_set_bitmap(s_background->bitmap_layer, s_background->bitmap);
 
-  s_hour1->position = 0;
-  s_hour2->position = 1;
-  s_minute1->position = 2;
-  s_minute2->position = 3;
+  layer_add_to_window(s_background->parent_layer, window);
+  bitmap_layer_add_to_layer(s_background->bitmap_layer, s_background->parent_layer);
 
-  s_hour1->out_of_frame = true;
-  s_hour2->out_of_frame = true;
-  s_minute1->out_of_frame = true;
-  s_minute2->out_of_frame = true;
+  layer_set_update_proc(s_background->parent_layer, update_layer);
+  update_time();
 
-  s_hour1->parent_layer = layer_create(GRect(-144, 0, BOX_X, BOX_Y));
-  s_hour2->parent_layer = layer_create(GRect(72, -168, BOX_X, BOX_Y));
-  s_minute1->parent_layer = layer_create(GRect(0, 252, BOX_X, BOX_Y));
-  s_minute2->parent_layer = layer_create(GRect(216, 84, BOX_X, BOX_Y));
+  // Initialize digit layers
+  for (int i = 0; i < 4; i++)
+  {
+    DigitLayer *digit = digits[i];
+    digit->position = i;
+    digit->out_of_frame = true;
 
-  s_material_layer = bitmap_layer_create(bounds);
-  s_material_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
-  bitmap_layer_set_bitmap(s_material_layer, s_material_bitmap);
+    digit->material.parent_layer = layer_create(GRect(
+        DIGIT_POSITION_VALUES[i].out_of_frame[0],
+        DIGIT_POSITION_VALUES[i].out_of_frame[1],
+        BOX_X, BOX_Y));
 
-  s_hour1->bitmap_layer = bitmap_layer_create(GRect(0, 0, BOX_X, BOX_Y));
-  s_hour2->bitmap_layer = bitmap_layer_create(GRect(0, 0, BOX_X, BOX_Y));
-  s_minute1->bitmap_layer = bitmap_layer_create(GRect(0, 0, BOX_X, BOX_Y));
-  s_minute2->bitmap_layer = bitmap_layer_create(GRect(0, 0, BOX_X, BOX_Y));
+    digit->material.bitmap_layer = bitmap_layer_create(GRect(0, 0, BOX_X, BOX_Y));
 
-  layer_add_to_window(s_background_layer, window);
+    layer_add_to_layer(digit->material.parent_layer, s_background->parent_layer);
+    bitmap_layer_add_to_layer(digit->material.bitmap_layer, digit->material.parent_layer);
 
-  bitmap_layer_add_to_layer(s_material_layer, s_background_layer);
-
-  layer_add_to_layer(s_hour1->parent_layer, s_background_layer);
-  layer_add_to_layer(s_hour2->parent_layer, s_background_layer);
-  layer_add_to_layer(s_minute1->parent_layer, s_background_layer);
-  layer_add_to_layer(s_minute2->parent_layer, s_background_layer);
-
-  bitmap_layer_add_to_layer(s_hour1->bitmap_layer, s_hour1->parent_layer);
-  bitmap_layer_add_to_layer(s_hour2->bitmap_layer, s_hour2->parent_layer);
-  bitmap_layer_add_to_layer(s_minute1->bitmap_layer, s_minute1->parent_layer);
-  bitmap_layer_add_to_layer(s_minute2->bitmap_layer, s_minute2->parent_layer);
-
-  do_animation(s_hour1);
-  do_animation(s_hour2);
-  do_animation(s_minute1);
-  do_animation(s_minute2);
+    do_animation(digits[i]);
+  }
 }
 
+/**
+ * Main window unload handler
+ * @param window The window being unloaded
+ */
 static void main_window_unload(Window *window)
 {
-  layer_destroy_safe(s_background_layer);
+  DigitLayer *digits[] = {s_hour1, s_hour2, s_minute1, s_minute2};
 
-  layer_destroy_safe(s_hour1->parent_layer);
-  layer_destroy_safe(s_hour2->parent_layer);
-  layer_destroy_safe(s_minute1->parent_layer);
-  layer_destroy_safe(s_minute2->parent_layer);
+  // Deinit digit layers
+  for (int i = 0; i < 4; i++)
+  {
+    layer_destroy_safe(digits[i]->material.parent_layer);
+    bitmap_layer_destroy_safe(digits[i]->material.bitmap_layer);
+    gbitmap_destroy_safe(digits[i]->material.bitmap);
+  }
 
-  bitmap_layer_destroy_safe(s_hour1->bitmap_layer);
-  bitmap_layer_destroy_safe(s_hour2->bitmap_layer);
-  bitmap_layer_destroy_safe(s_minute1->bitmap_layer);
-  bitmap_layer_destroy_safe(s_minute2->bitmap_layer);
-  bitmap_layer_destroy_safe(s_material_layer);
-
-  gbitmap_destroy_safe(s_hour1->bitmap);
-  gbitmap_destroy_safe(s_hour2->bitmap);
-  gbitmap_destroy_safe(s_minute1->bitmap);
-  gbitmap_destroy_safe(s_minute2->bitmap);
-  gbitmap_destroy_safe(s_material_bitmap);
+  layer_destroy_safe(s_background->parent_layer);
+  bitmap_layer_destroy_safe(s_background->bitmap_layer);
+  gbitmap_destroy_safe(s_background->bitmap);
 }
 
+/**
+ * Initialize the app
+ */
 static void init()
 {
   s_main_window = window_create();
@@ -286,6 +306,9 @@ static void init()
   timer = app_timer_register(180 * 1000, timer_callback, NULL);
 }
 
+/**
+ * Deinitialize the app
+ */
 static void deinit()
 {
   animation_unschedule_all();
@@ -294,6 +317,9 @@ static void deinit()
   window_destroy_safe(s_main_window);
 }
 
+/**
+ * Main entry point
+ */
 int main(void)
 {
   init();
